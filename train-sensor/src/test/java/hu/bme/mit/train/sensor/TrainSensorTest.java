a298e0fbe468ab14941b26b3f930b595dc1f9bb9
Bakos Ádám <drmook9@gmail.com>
2019-02-04 07:48:07 -0800

    fixed

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -7,26 +7,33 @@ import static org.mockito.Mockito.*;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.TrainSystem;
 
 
 
 public class TrainSensorTest {
 
+    TrainController controller;
+    TrainSensor sensor;
+    TrainUser user;
+
+
+
     @Before
     public void before() {
-        // TODO Add initializations
+        TrainSystem system = new TrainSystem();
+        controller = system.getController();
+        sensor = system.getSensor();
+        user = system.getUser();
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
 
-
-	TrainController controller;
-	TrainUser user;
-    TrainSensorImpl tsi=new TrainSensorImpl(controller, user);
-
-	assertEquals(5,tsi.getSpeedLimit(), "default value must be 5");    
+        Assert.assertEquals(10, controller.getReferenceSpeed());
+        controller.followSpeed();
+        Assert.assertEquals(10, controller.getReferenceSpeed());
+    }
 
 
     }
-}
